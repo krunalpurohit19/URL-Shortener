7a357fa27ad3a9739117d94cac5e71d8930992ed
kpurohit009 <kpurohit009@gmail.com>
2025-07-30 15:11:58 +0530

    add baseURL

--- Frontend/src/utils/axiosInstance.jsx
+++ Frontend/src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://url-shortener-vert-seven.vercel.app/api',
     timeout: 10000, // Set a timeout for requests
     withCredentials: true,
 });
