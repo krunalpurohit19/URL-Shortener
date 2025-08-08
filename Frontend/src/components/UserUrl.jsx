a41f182f77194da079720e6e563d2352cc17391f
kpurohit009 <kpurohit009@gmail.com>
2025-08-08 17:37:34 +0530

    production bug fix

--- Frontend/src/components/UserUrl.jsx
+++ Frontend/src/components/UserUrl.jsx
@@ -80,11 +80,12 @@ const UserUrl = () => {
                                 <td className="px-6 py-4">
                                     <div className="text-sm">
                                         <a
-                                            href={url.short_url}
+                                            href={`https://makeurlinks.vercel.app/${url.short_url}`}
                                             target="_blank"
                                             rel="noopener noreferrer"
                                             className="text-blue-600 hover:text-blue-900 hover:underline"
                                         >
+                                            {console.log(url.short_url)}
                                             {`makeurlinks.vercel.app/${url.short_url}`}
                                         </a>
                                     </div>
