34f219ca9608e18a8e13bf002585085e9ce58d2f
kpurohit009 <kpurohit009@gmail.com>
2025-09-28 19:19:18 +0530

    update timeout in axiosInstance

--- Frontend/src/utils/axiosInstance.jsx
+++ Frontend/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://url-shortener-a7ry.onrender.com/api',
-    timeout: 30000, // Set a timeout for requests
+    timeout: 60000, // Set a timeout for requests
     withCredentials: true,
 });
 
